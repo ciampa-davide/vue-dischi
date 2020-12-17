78fe82cd7c4647c637868a4a4f16920adab61e6c
Davide <ciampad06@gmail.com>
2020-12-17 16:51:53 +0100

    bonus 2

--- js/script.js
+++ js/script.js
@@ -10,12 +10,15 @@ var app= new Vue(
 
     mounted: function(){
       const self = this;
-
       axios
       .get("https://flynn.boolean.careers/exercises/api/array/music")
       .then( function(result){
-
-        self.disks = result.data.response;
+        //ordino i dischi per data
+        let disk = result.data.response;
+        disk.sort(function(a, b){
+          return a.title - b.title;
+        });
+        self.disks = disk;
         console.log(self.disks);
         }
       )
